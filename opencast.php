5de9d4eb190bd86483b2a16d8635285fb8d7602b
farbod <zamanifarbod2@gmail.com>
2021-01-27 11:03:21 +0100

    correction: ## Incorrect Copyright

--- opencast.php
+++ opencast.php
@@ -27,11 +27,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
-Copyright 2005-2015 Automattic, Inc.
- */
+along with this program
+*/
 
 // Make sure we don't expose any info if called directly
 defined('ABSPATH') or die('This is a pluging, direct call is prevented!');
